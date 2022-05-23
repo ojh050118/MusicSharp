ce5c6cfa1be47cdc819d7d3e5b8b0181b70bc143
ojh050118 <ojh0050118@gmail.com>
2022-05-23 23:18:57 +0900

    Add Join, (Leave) command

---
+++ MusicSharp.Game/Commands/Music/MusicInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicSharp.Game.Commands.Music
+{
+    public static class MusicInfo
+    {
+        public static Queue<string> Queue = new Queue<string>();
+
+        public static string NowPlaying;
+    }
+}
